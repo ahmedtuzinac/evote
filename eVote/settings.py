b9c9c66648f75f83a95a514d160c4044217aa001
Ahmed Tuzinac <tuzinac96@gmail.com>
2023-01-28 09:39:12 +0100

    commit

--- eVote/settings.py
+++ eVote/settings.py
@@ -1,5 +1,7 @@
 
 from pathlib import Path
+import os 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,7 +80,7 @@ DATABASES = {
         'NAME': 'evotedb',
         'PORT':'5433',
         'USER':'postgres',
-        'PASSWORD':'ahmed',
+        'PASSWORD': 'ahmed',
         'HOST':'localhost',
     }
 }
